73a86ef4356e486eb115740f96ae43a5ad02ef58
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2020-01-06 03:29:03 +0500

    Update vi2au.py

--- vi2au.py
+++ vi2au.py
@@ -1,138 +1,140 @@
 import os
-import sys
-import pipes
-import logging
+from sys import argv
 from glob import glob
+from pipes import quote
 from shutil import move
+from logging import (basicConfig, error, debug, warning, info, DEBUG, WARN, ERROR)
+
+basicConfig(format="%(levelname)s:%(message)s", level=DEBUG, filename='info.log')
+
+class Collector():
+    """
+    Usage:
+    python3 [vi2au.py] [-d DIR [DIR ...]] [-m MEDIA_NAME [MEDIA_NAME ...]] [-o OUTPUT_DIR]
+    python3 [vi2au.py] [-d DIR] [-f FORMAT [FORMAT ...]] [-o OUTPUT_DIR]
+
+    Options and arguments (and corresponding environment variables):
+        -h, --help                            Show this help message.
+        -d DIR [DIR ...]                      Convert specified/all files in a specified directory.
+                                            (if none specified => defaults to current directory)
+        -f FORMAT [FORMAT ...]                Convert specific format media.
+        -m MEDIA_NAME [MEDIA_NAME ...]        Provide media name.
+        -o OUTPUT_DIR                         Output directory.
+    """
+    
+    def __init__(self):
+        self.clear = ('cls' if os.name == 'nt' else 'clear')
+        self.current_dir = os.getcwd()
+        self.__formats__ = self.collect_spec('-f', '*')
+        self.__files__ = self.collect_spec('-m')
+        self.__dirs__ = self.collect_spec('-d')
+        self.__outdir__ = self.collect_spec('-o', '~/Music')
+
+        self.collection = []
+        self._garbage = []
+
+        self.collect_files()
+        self.FILES = len(self.collection)
+
+    def __str__(self):
+        return str(self.collection)
+
+    def collect_spec(self, spec, non_empty=''):
+        """
+        Collects specifier arguments from provided sys.argv
+        and return list of arguments in given space.
+
+        spec : (string) => ('-f', '-m', '-d', '-o')
+        non_empty : (string/char) => if no such spec found, return non empty list [string/char]
+        """
+        # Collect 'spec' arguments from sys.argv
+        collection = []
+        if spec in argv:
+            for arg in argv[argv.index(spec) + 1:]:
+                # Append till next specifier
+                if not arg.startswith('-'): 
+                    collection.append(arg)
+                else:
+                    break
+
+        if non_empty and not collection:
+            collection.append(non_empty if (non_empty == '*') else os.path.expanduser(non_empty))
+        
+        return collection
+
+    def _get_files(self, dir):
+        # Invalid path
+        if not os.path.exists(dir): return
+
+        os.chdir(dir)
+        for file_format in self.__formats__:
+            for file in glob('*.{}'.format(file_format)):
+                self.collection.append(os.path.join(dir, file))
+
+        os.chdir(self.current_dir)
+            
+    def collect_files(self):
+        # Collect specified file(s) in specified dir(s)
+        if self.__dirs__ and self.__files__:
+            for dir in self.__dirs__:
+                for file in self.__files__:
+                    self.collection.append(os.path.join(dir, file))
+
+        # Collect all file(s) in specified dir(s)
+        elif self.__dirs__:
+            for dir in self.__dirs__:
+                self._get_files(dir)
+
+        # Collect specified file(s) in current dir(s)
+        elif self.__files__:
+            for file in self.__files__:
+                    self.collection.append(os.path.join(self.current_dir, file))
+
+        # Collect all file(s) in current dir(s)
+        elif not self.__dirs__ and ('-d' in argv or '-f' in argv):
+            self._get_files(self.current_dir)
+
+    def mov_to_dir(self, audio_dir, ext):
+        if not os.path.exists(audio_dir):
+            os.mkdir(audio_dir)
+
+        for audio_file in glob('*.{}'.format(ext)):
+            os.system(self.clear)
+            debug("Moving file '{}' to {}\ ".format(audio_file, audio_dir))
+            move(audio_file, audio_dir)
+
+class VideoToMp3(Collector):
+    def __init__(self):
+        super().__init__()
+        os.system(self.clear)
+        debug("Files found = {}".format(self.FILES))
+
+    def __str__(self):
+        return super().__str__()
+
+    def splitName(self, filename):
+        path = os.path.dirname(filename)
+        file = os.path.basename(filename)
+        filename, extention = os.path.splitext(file)
+        return (path, filename, extention)
+
+    def convert(self, out_extention='mp3'):
+        for file in self.collection:
+            info('Converting file = {}'.format(file))
+            path, filename, extention = self.splitName(file)
+            debug("{} | {} | {}".format(path, filename, extention))
+            command = 'ffmpeg -i "' + file + '" "' + filename+'.'+out_extention + '"'
+            debug("command = " + command)
+            os.system(command)
+
+        for dir in self.__outdir__:
+            debug("Moving {} file(s) to '{}'".format(out_extention, dir))
+            self.mov_to_dir(dir, out_extention)
 
 
-clear = ('cls' if os.name == 'nt' else 'clear')
+if __name__ == "__main__":
+    converter = VideoToMp3()
+    converter.convert()
 
-logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG, filename='info.log')
-
-def video_to_audio(fileName):
-	"""
-	Usage:
-	python3 [vi2au.py] [-d DIR] [-m MEDIA_NAME [MEDIA_NAME ...]] [-o OUTPUT_DIR]
-	python3 [vi2au.py] [-d DIR] [-f FORMAT] [-o OUTPUT_DIR]
-
-	Options and arguments (and corresponding environment variables):
-	  -h, --help		Show this help message.
-	  -m MEDIA_NAME [MEDIA_NAME ...]		Provide media name.
-	  -d DIR		Convert all files in a directory.
-					(if none specified => defaults to current directory)
-	  -o OUTPUT_DIR		Output directory.
-	  -f FORMAT		Convert specific format media.
-
-	"""
-	try:
-		os.system(clear)
-		file, file_extension = os.path.splitext(fileName)
-		logging.info("Filename split to {} and {}".format(file, file_extension))
-		file = pipes.quote(file)
-		video_to_wav = 'ffmpeg -i ' + file + file_extension + ' ' + file + '.wav'
-		logging.info("Converting to = {}".format(file[1:-1] + '.wav'))
-		os.system(video_to_wav)
-
-		os.system(clear)
-		final_audio = 'lame '+ file + '.wav' + ' ' + file + '.mp3'
-		logging.info("Final output filename = {}".format(file[1:-1] + '.mp3'))
-		os.system(final_audio)
-
-		os.system(clear)
-		file = file[1:-1] + '.wav'
-		logging.debug("Removing file '{}'".format(file))
-		os.remove(file)
-		logging.info("Sucessfully converted '{}' into audio!".format(fileName))
-		return True
-
-	except Exception as err:
-		logging.error("During conversion an error occurred => {}".format(err))
-		logging.info("File not converted/removed -> {}".format(fileName))
-		pass
-
-	return False
-
-def main():
-	file_count = 0
-	files = []
-	if ('-d' in sys.argv):
-		i = sys.argv.index('-d')
-		os.chdir(sys.argv[i + 1]) if os.path.exists(sys.argv[i + 1]) else os.getcwd()
-
-	if ('-m' in sys.argv):
-		i = sys.argv.index('-m')
-		for arg in sys.argv[i + 1:]:
-			if not arg.startswith('-'):
-				files.append(arg)
-			else:
-				break
-
-	if ('-f' in sys.argv):
-		i = sys.argv.index('-f')
-		for arg in sys.argv[i + 1:]:
-			if not arg.startswith('-'):
-				for file in glob('*.{}'.format(arg)):
-					files.append(file)
-			else:
-				break
-
-	if not files:
-		for file in os.listdir():
-			if os.path.isfile(os.path.join(os.getcwd(), file)):
-				files.append(file)
-
-	logging.info("Total file(s) to convert : {}".format(len(files)))
-	logging.info("File(s) to convert : {}".format(files))
-
-	for filePath in files:
-		# check if the specified file exists or not
-		try:
-			if os.path.exists(filePath):
-				logging.info("File received and path exists = {}".format(filePath))
-
-		except OSError as err:
-			logging.error("File found but no such path exists = {}".format(filePath))
-			pass
-
-		# convert video to audio
-		logging.info("Sending file for conversion to procedure '{}'".format(video_to_audio))
-		if video_to_audio(filePath):
-			file_count += 1
-
-	logging.info("Files converted = {}".format(file_count))
-	return file_count
-
-def mov_to_dir(audio_dir):
-	if not os.path.exists(os.path.join(os.getcwd(), audio_dir)):
-		os.mkdir(audio_dir)
-
-	for mp3File in glob('*.mp3'):
-		os.system(clear)
-		logging.info("Moving file '{}' to {}\ ".format(mp3File, audio_dir))
-		move(mp3File, audio_dir)
-
-	return
-
-# install ffmpeg and/or lame if you get an error saying that the program is currently not installed
-if __name__ == '__main__':
-	if '-h' in sys.argv or '--help' in sys.argv:
-		help(video_to_audio)
-		exit()
-
-	try:
-		file_count = main()
-		if file_count > 0:
-			output_dir = 'Audio'
-			if '-o' in sys.argv:
-				i = sys.argv.index('-o')
-				output_dir = sys.argv[i + 1]
-
-			mov_to_dir(output_dir)
-			# if .wav file(s) not removed
-			for file in glob('*.wav'):
-				os.remove(file)
-
-	except Exception as exp:
-		print(exp)
-		help(video_to_audio)
+    if '-h' in argv or '--help' in argv:
+        help(VideoToMp3)
